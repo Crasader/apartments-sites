d81236945822e7a118cab3f654ba5ea73ebaf6e4
amcllc mktserver digital-ocean <wmeraflen+1@gmail.com>
2017-05-05 03:12:04 +0000

    Assets for material

---
+++ laravel/database/migrations/2017_05_05_013402_material_template_insert_record.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MaterialTemplateInsertRecord extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        $templates = app()->make('App\Template');
+        if(count($templates->where('name','material')->get())){
+            echo "Material template already in db...exiting" . PHP_EOL;
+            return;
+        }
+
+        $templates = new \App\Template;
+        $templates->name = $templates->filesystem_id = 'material';
+        $templates->save();
+        echo "Saved template : MATERIAL" . PHP_EOL;
+        return;
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
