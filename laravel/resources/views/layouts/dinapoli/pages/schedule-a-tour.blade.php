b03007efc12b9a99c895cd2e9d640a54eb0883bb
brady <bvfbarten@gmail.com>
2017-05-17 16:27:05 -0600

    fixed map

--- laravel/resources/views/layouts/dinapoli/pages/schedule-a-tour.blade.php
+++ laravel/resources/views/layouts/dinapoli/pages/schedule-a-tour.blade.php
@@ -1,4 +1,8 @@
-<?php use App\Util\Util; ?>
+<?php
+use App\Util\Util;
+use App\Property\Template as PropertyTemplate;
+?>
+
 @extends('layouts/dinapoli/main')
             @section('extra-css')
             <link rel="stylesheet" href="css/bootstrap-datepicker3.min.css"/>
@@ -29,7 +33,7 @@
                     ?>
                     <div class="section-text mb-50 mb-sm-20">
                         <div class="row">
-                            
+
                             <div class="col-md-7 col-sm-7 mb-sm-50 mb-xs-30">
                                 <form role="form" id="form1" name="form1" method="post" class="validate" action="/schedule">
                                     <?php //TODO: Find a form helper for laravel that will do this for us ?>
@@ -80,7 +84,7 @@
                                     </div>
                                 </form>
                             </div>
-                            
+
                             <div class="col-md-5 col-sm-5 mb-sm-50 mb-xs-30 text-center">
                                 <div class="row">
                                 	<div class="col-sm-12">
@@ -88,7 +92,7 @@
                                             <div class="map-block">
                                                 <div class="map">
                                                     <div class="map-container">
-                                                        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script> 
+                                                        <?=PropertyTemplate::getGmapKey();?>
                                                         <div style="height:537px;overflow:hidden;max-width:100%;">
                                                             <div id="map-canvas" style="max-width:100%;"></div>
                                                         <div>
@@ -101,7 +105,7 @@
                             </div>
                         </div>
                     </div>
-                    
+
                 </div>
             </section>
             <!-- End Schedule Section -->
@@ -110,12 +114,12 @@
             @section('contact')
             <section class="page-section pt-0" id="contact">
                 <div class="container relative">
-                    
+
                     <div class="row">
-                        
+
                         <div class="col-md-10 col-md-offset-1">
                             <div class="row">
-                                
+
                                 <!-- Phone -->
                                 <div class="col-sm-6 col-lg-4 pt-20 pb-20 pb-xs-20">
                                     <div class="contact-item">
@@ -131,7 +135,7 @@
                                     </div>
                                 </div>
                                 <!-- End Phone -->
-                                
+
                                 <!-- Address -->
                                 <div class="col-sm-6 col-lg-4 pt-20 pb-20 pb-xs-20">
                                     <div class="contact-item">
@@ -147,7 +151,7 @@
                                     </div>
                                 </div>
                                 <!-- End Address -->
-                                
+
                                 <!-- Office Hours -->
                                 <div class="col-sm-6 col-lg-4 pt-20 pb-20 pb-xs-20">
                                     <div class="contact-item">
@@ -166,7 +170,7 @@
                             </div>
                         </div>
                     </div>
-            	</div>        
+            	</div>
             </section>
             @stop
 
@@ -174,15 +178,15 @@
 		<script type="text/javascript">
 		$(document).ready(function() {
             <?php
-                /* TODO: Add custom messages for each field 
+                /* TODO: Add custom messages for each field
                  * TODO: Create validation that doesn't let the user enter a time before or after the valid hours of the office
                  * TODO: Create a "slot" or something that we can utilize so that we don't have to duplicate this form code everywhere
                  */
             ?>
             $("#timepicker").timepicker({});
-            $("#datepicker").datepicker({format: 'mm/dd/yyyy'});	
-            $("#moveindatediv").datepicker({format: 'mm/dd/yyyy'});	
-            amcBindValidate({ 
+            $("#datepicker").datepicker({format: 'mm/dd/yyyy'});
+            $("#moveindatediv").datepicker({format: 'mm/dd/yyyy'});
+            amcBindValidate({
                 'form': '#form1',
                 'ignore': '.ignore',
                 'rules': {
@@ -221,6 +225,6 @@
             });
             amcMaskPhone('#phone','(999) 999-9999');
 		});	//End document.ready
-		
+
 	</script>
 		@stop
