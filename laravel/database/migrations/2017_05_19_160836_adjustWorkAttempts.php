9202b627ebb5b9ecf838a55c6ec358a899c6afea
brady <bvfbarten@gmail.com>
2017-05-19 10:21:28 -0600

    adjusted queue tries to allow for more tries

---
+++ laravel/database/migrations/2017_05_19_160836_adjustWorkAttempts.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AdjustWorkAttempts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('jobs', function($table){
+            $table->integer('attempts')
+                ->unsigned()
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
