5e41d2031eabaf36bd2d54dc39bd6d1a0786cca5
amcllc mktserver digital-ocean <wmeraflen+1@gmail.com>
2017-05-23 19:51:03 +0000

    system settings migration

---
+++ laravel/database/migrations/2017_05_23_194858_create_system_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSystemSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(Schema::hasTable('system_settings')){ return; }
+        Schema::create('system_settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('fk_property_id');
+            $table->string('str_key');
+            $table->text('str_value');
+            $table->string('scope');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('system_settings');
+    }
+}
