baec7c77cc86bf1da15a92b8b37c769e201f284b
brady <bvfbarten@gmail.com>
2017-05-30 10:34:59 -0600

    added onepage

---
+++ laravel/resources/views/layouts/onepage/onepage/js/custom.js
@@ -0,0 +1,412 @@
+			$(function(){
+
+				/***************** Mobile Nav ******************/
+
+			    $(".mobile-nav").hide();
+			    $("#mobile-toggle").on("click", function() {
+			      $(".mobile-nav").fadeToggle();
+			      $("#mobile-toggle").toggleClass("collapsed");
+			    });
+			    $(".mobile-nav-link").on("click", function() {
+			      $(".mobile-nav").fadeToggle();
+			      $("#mobile-toggle").toggleClass("collapsed");
+			    });
+
+				/***************** Slider ******************/
+
+				$('.slippry').slippry({
+
+					pager: false,
+					controls: true,
+					transition: 'horizontal',
+				});
+
+				$('.fgallery').featherlightGallery({
+					gallery: {
+						fadeIn: 300,
+						fadeOut: 300
+					},
+					openSpeed:    300,
+					closeSpeed:   300
+				});
+
+				$(document).ready(function(){
+				    $(".bottom-callout").animate({bottom:'0px'},1000, 'easeOutBounce');
+				});
+
+				
+			});
+var mr = (function ($, window, document){
+    "use strict";
+
+    var mr         = {},
+        components = {documentReady: [],documentReadyDeferred: [], windowLoad: [], windowLoadDeferred: []};
+
+
+    $(document).ready(documentReady);
+    $(window).on("load", windowLoad);
+
+    function documentReady(context){
+        
+        context = typeof context === typeof undefined ? $ : context;
+        components.documentReady.concat(components.documentReadyDeferred).forEach(function(component){
+            component(context);
+        });
+    }
+
+    function windowLoad(context){
+        
+        context = typeof context === "object" ? $ : context;
+        components.windowLoad.concat(components.windowLoadDeferred).forEach(function(component){
+           component(context);
+        });
+    }
+
+    mr.setContext = function (contextSelector){
+        var context = $;
+        if(typeof contextSelector !== typeof undefined){
+            return function(selector){
+                return $(contextSelector).find(selector);
+            };
+        }
+        return context;
+    };
+
+    mr.components    = components;
+    mr.documentReady = documentReady;
+    mr.windowLoad    = windowLoad;
+
+    return mr;
+}(jQuery, window, document));
+
+
+//////////////// Utility Functions
+mr = (function (mr, $, window, document){
+    "use strict";
+    mr.util = {};
+
+    mr.util.requestAnimationFrame    = window.requestAnimationFrame || 
+                                       window.mozRequestAnimationFrame || 
+                                       window.webkitRequestAnimationFrame ||
+                                       window.msRequestAnimationFrame;
+
+    mr.util.documentReady = function($){
+        var today = new Date();
+        var year = today.getFullYear();
+        $('.update-year').text(year);
+    };
+
+    mr.util.getURLParameter = function(name) {
+        return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [undefined, ""])[1].replace(/\+/g, '%20')) || null;
+    };
+
+
+    mr.util.capitaliseFirstLetter = function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
+    mr.util.slugify = function(text, spacesOnly){
+        if(typeof spacesOnly !== typeof undefined){
+            return text.replace(/ +/g, '');
+        }else{
+            return text
+                .toLowerCase()
+                .replace(/[\~\!\@\#\$\%\^\&\*\(\)\-\_\=\+\]\[\}\{\'\"\;\\\:\?\/\>\<\.\,]+/g, '')
+                .replace(/ +/g, '-');
+        }
+    };
+
+    mr.util.sortChildrenByText = function(parentElement, reverse){
+        var $parentElement = $(parentElement);
+        var items          = $parentElement.children().get();
+        var order          = -1;
+        var order2         = 1;
+        if(typeof reverse !== typeof undefined){order = 1; order2 = -1;}
+
+        items.sort(function(a,b){
+          var keyA = $(a).text();
+          var keyB = $(b).text();
+
+          if (keyA < keyB) return order;
+          if (keyA > keyB) return order2;
+          return 0;
+        });
+        
+        // Append back into place
+        $parentElement.empty();
+        $(items).each(function(i, itm){
+          $parentElement.append(itm);
+        });
+    };
+    
+    // Set data-src attribute of element from src to be restored later
+    mr.util.idleSrc = function(context, selector){
+        
+            selector  = (typeof selector !== typeof undefined) ? selector : '';
+            var elems = context.is(selector+'[src]') ? context : context.find(selector+'[src]');
+
+        elems.each(function(index, elem){
+            elem           = $(elem);
+            var currentSrc = elem.attr('src'),
+                dataSrc    = elem.attr('data-src');
+
+            // If there is no data-src, save current source to it
+            if(typeof dataSrc === typeof undefined){
+                elem.attr('data-src', currentSrc);
+            }
+
+            // Clear the src attribute
+            elem.attr('src', '');    
+            
+        });
+    };
+
+    // Set src attribute of element from its data-src where it was temporarily stored earlier
+    mr.util.activateIdleSrc = function(context, selector){
+        
+        selector     = (typeof selector !== typeof undefined) ? selector : '';
+        var elems    = context.is(selector+'[src]') ? context : context.find(selector+'[src]');
+
+        elems.each(function(index, elem){
+            elem = $(elem);
+            var dataSrc    = elem.attr('data-src');
+
+            // If there is no data-src, save current source to it
+            if(typeof dataSrc !== typeof undefined){
+                elem.attr('src', dataSrc);
+            }
+        });
+    };
+
+    mr.util.pauseVideo = function(context){
+        var elems = context.is('video') ? context : context.find('video');
+
+        elems.each(function(index, video){
+            var playingVideo = $(video).get(0);
+            playingVideo.pause();
+        });
+    };
+
+    // Take a text value in either px (eg. 150px) or vh (eg. 65vh) and return a number in pixels.
+    mr.util.parsePixels = function(text){
+        var windowHeight = $(window).height(), value;
+        
+        // Text text against regular expression for px value.
+        if(/^[1-9]{1}[0-9]*[p][x]$/.test(text)){
+            return parseInt(text.replace('px', ''),10);
+        }
+        // Otherwise it is vh value.
+        else if(/^[1-9]{1}[0-9]*[v][h]$/.test(text)){
+            value = parseInt(text.replace('vh', ''),10);
+            // Return conversion to percentage of window height.
+            return windowHeight * (value/100);
+        }else{
+            // If it is not proper text, return -1 to indicate bad value.
+            return -1;
+        }
+    };
+
+    mr.components.documentReady.push(mr.util.documentReady);
+    return mr;
+
+}(mr, jQuery, window, document));
+
+
+//////////////// Scroll Functions
+mr = (function (mr, $, window, document){
+    "use strict";
+
+    mr.scroll           = {};
+    mr.scroll.listeners = [];
+    mr.scroll.y         = 0;
+    mr.scroll.x         = 0;
+
+     var documentReady = function($){
+        
+        // Check if scroll-assist is on
+        if($('body').hasClass('scroll-assist')){
+            mr.scroll.assisted = true;
+        }
+
+        //////////////// Capture Scroll Event and fire scroll function
+        
+        addEventListener('scroll', function(evt) {        
+                //if(!mr.scroll.assisted){
+                    window.mr.scroll.y = window.pageYOffset;
+                //}
+                window.mr.scroll.update(evt);
+        }, false);
+        
+    };
+
+    mr.scroll.update = function(event){
+        // Loop through all mr scroll listeners
+        for (var i = 0, l = mr.scroll.listeners.length; i < l; i++) {
+           mr.scroll.listeners[i](event);
+        }
+    };
+
+    mr.scroll.documentReady = documentReady;
+
+    mr.components.documentReady.push(documentReady);
+
+    return mr;
+
+}(mr, jQuery, window, document));
+
+
+//////////////// Scroll Class Modifier
+mr = (function (mr, $, window, document){
+    "use strict";
+
+    mr.scroll.classModifiers = {};
+    // Globally accessible list of elements/rules
+    mr.scroll.classModifiers.rules = [];
+
+    mr.scroll.classModifiers.parseScrollRules = function(element){
+        var text  = element.attr('data-scroll-class'),
+            rules = text.split(";");
+
+        rules.forEach(function(rule){
+            var ruleComponents, scrollPoint, ruleObject = {};
+            ruleComponents = rule.replace(/\s/g, "").split(':');
+            if(ruleComponents.length === 2){
+                scrollPoint = mr.util.parsePixels(ruleComponents[0]);
+                if(scrollPoint > -1){
+                    ruleObject.scrollPoint = scrollPoint;
+                    if(ruleComponents[1].length){
+                        var toggleClass = ruleComponents[1];
+                        ruleObject.toggleClass = toggleClass;
+                        // Set variable in object to indicate that element already has class applied
+                        ruleObject.hasClass = element.hasClass(toggleClass);
+                        ruleObject.element = element.get(0);
+                        mr.scroll.classModifiers.rules.push(ruleObject);
+                    }else{
+                        // Error: toggleClass component does not exist.
+                        //console.log('Error - toggle class not found.');
+                        return false;
+                    }
+                }else{
+                    // Error: scrollpoint component was malformed
+                    //console.log('Error - Scrollpoint not found.');
+                    return false;
+                }
+            }   
+        });
+        
+        if(mr.scroll.classModifiers.rules.length){
+            return true;
+        }else{
+            return false;
+        }
+    };
+
+    mr.scroll.classModifiers.update = function(event){
+        var currentScroll = mr.scroll.y,
+            scrollRules   = mr.scroll.classModifiers.rules,
+            l             = scrollRules.length,
+            currentRule;
+        
+        // Given the current scrollPoint, check for necessary changes 
+        while(l--) {
+            
+            currentRule = scrollRules[l];
+            
+            if(currentScroll > currentRule.scrollPoint && !currentRule.hasClass){
+                // Set local copy and glogal copy at the same time;
+                currentRule.element.classList.add(currentRule.toggleClass);
+                currentRule.hasClass = mr.scroll.classModifiers.rules[l].hasClass = true;
+            }
+            if(currentScroll < currentRule.scrollPoint && currentRule.hasClass){
+                // Set local copy and glogal copy at the same time;
+                currentRule.element.classList.remove(currentRule.toggleClass);
+                currentRule.hasClass = mr.scroll.classModifiers.rules[l].hasClass = false;
+            }
+        }
+    };
+
+    var fixedElementSizes = function(){
+        $('.main-container [data-scroll-class*="pos-fixed"]').each(function(){
+            var element = $(this);
+            element.css('max-width',element.parent().outerWidth());
+            element.parent().css('min-height',element.outerHeight());
+        });
+    };
+
+    var documentReady = function($){
+        // Collect info on all elements that require class modification at load time
+        // Each element has data-scroll-class with a formatted value to represent class to add/remove at a particular scroll point.
+        $('[data-scroll-class]').each(function(){
+            var element  = $(this);
+                
+            // Test the rules to be added to an array of rules.
+            if(!mr.scroll.classModifiers.parseScrollRules(element)){
+                console.log('Error parsing scroll rules on: '+element);
+            }
+        });
+
+        // For 'position fixed' elements, give them a max-width for correct fixing behaviour
+        fixedElementSizes();
+        $(window).on('resize', fixedElementSizes);
+        
+        // If there are valid scroll rules add classModifiers update function to the scroll event processing queue.
+        if(mr.scroll.classModifiers.rules.length){
+            mr.scroll.listeners.push(mr.scroll.classModifiers.update);
+        }
+    };
+
+    mr.components.documentReady.push(documentReady);
+    mr.scroll.classModifiers.documentReady = documentReady;    
+
+    
+
+    return mr;
+
+}(mr, jQuery, window, document));
+
+
+
+			//////////////// Bars
+mr = (function (mr, $, window, document){
+    "use strict";
+    
+    var documentReady = function($){
+        $('.nav-container .bar[data-scroll-class*="fixed"]:not(.bar--absolute)').each(function(){
+            var bar = $(this),
+                barHeight = bar.outerHeight(true);
+            bar.closest('.nav-container').css('min-height',barHeight);
+        });
+    };
+
+    mr.bars = {
+        documentReady : documentReady        
+    };
+
+    mr.components.documentReady.push(documentReady);
+    return mr;
+
+}(mr, jQuery, window, document));
+
+/*! Copyright (c) Go Make Things, LLC
+
+https://github.com/cferdinandi/smooth-scroll
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+!function(a,b){"function"==typeof define&&define.amd?define([],b(a)):"object"==typeof exports?module.exports=b(a):a.smoothScroll=b(a)}("undefined"!=typeof global?global:this.window||this.global,function(a){"use strict";var d,e,f,g,h,i,j,b={},c="querySelector"in document&&"addEventListener"in a,k={selector:"[data-scroll]",selectorHeader:null,speed:500,easing:"easeInOutCubic",offset:0,callback:function(){}},l=function(){var a={},b=!1,c=0,d=arguments.length;"[object Boolean]"===Object.prototype.toString.call(arguments[0])&&(b=arguments[0],c++);for(var e=function(c){for(var d in c)Object.prototype.hasOwnProperty.call(c,d)&&(b&&"[object Object]"===Object.prototype.toString.call(c[d])?a[d]=l(!0,a[d],c[d]):a[d]=c[d])};c<d;c++){var f=arguments[c];e(f)}return a},m=function(a){return Math.max(a.scrollHeight,a.offsetHeight,a.clientHeight)},n=function(a,b){var e,f,c=b.charAt(0),d="classList"in document.documentElement;for("["===c&&(b=b.substr(1,b.length-2),e=b.split("="),e.length>1&&(f=!0,e[1]=e[1].replace(/"/g,"").replace(/'/g,"")));a&&a!==document&&1===a.nodeType;a=a.parentNode){if("."===c)if(d){if(a.classList.contains(b.substr(1)))return a}else if(new RegExp("(^|\\s)"+b.substr(1)+"(\\s|$)").test(a.className))return a;if("#"===c&&a.id===b.substr(1))return a;if("["===c&&a.hasAttribute(e[0])){if(!f)return a;if(a.getAttribute(e[0])===e[1])return a}if(a.tagName.toLowerCase()===b)return a}return null},o=function(a){"#"===a.charAt(0)&&(a=a.substr(1));for(var e,b=String(a),c=b.length,d=-1,f="",g=b.charCodeAt(0);++d<c;){if(e=b.charCodeAt(d),0===e)throw new InvalidCharacterError("Invalid character: the input contains U+0000.");f+=e>=1&&e<=31||127==e||0===d&&e>=48&&e<=57||1===d&&e>=48&&e<=57&&45===g?"\\"+e.toString(16)+" ":e>=128||45===e||95===e||e>=48&&e<=57||e>=65&&e<=90||e>=97&&e<=122?b.charAt(d):"\\"+b.charAt(d)}return"#"+f},p=function(a,b){var c;return"easeInQuad"===a&&(c=b*b),"easeOutQuad"===a&&(c=b*(2-b)),"easeInOutQuad"===a&&(c=b<.5?2*b*b:-1+(4-2*b)*b),"easeInCubic"===a&&(c=b*b*b),"easeOutCubic"===a&&(c=--b*b*b+1),"easeInOutCubic"===a&&(c=b<.5?4*b*b*b:(b-1)*(2*b-2)*(2*b-2)+1),"easeInQuart"===a&&(c=b*b*b*b),"easeOutQuart"===a&&(c=1- --b*b*b*b),"easeInOutQuart"===a&&(c=b<.5?8*b*b*b*b:1-8*--b*b*b*b),"easeInQuint"===a&&(c=b*b*b*b*b),"easeOutQuint"===a&&(c=1+--b*b*b*b*b),"easeInOutQuint"===a&&(c=b<.5?16*b*b*b*b*b:1+16*--b*b*b*b*b),c||b},q=function(a,b,c){var d=0;if(a.offsetParent)do d+=a.offsetTop,a=a.offsetParent;while(a);return d=Math.max(d-b-c,0),Math.min(d,s()-r())},r=function(){return Math.max(document.documentElement.clientHeight,a.innerHeight||0)},s=function(){return Math.max(document.body.scrollHeight,document.documentElement.scrollHeight,document.body.offsetHeight,document.documentElement.offsetHeight,document.body.clientHeight,document.documentElement.clientHeight)},t=function(a){return a&&"object"==typeof JSON&&"function"==typeof JSON.parse?JSON.parse(a):{}},u=function(a){return a?m(a)+a.offsetTop:0},v=function(b,c,d){d||(b.focus(),document.activeElement.id!==b.id&&(b.setAttribute("tabindex","-1"),b.focus(),b.style.outline="none"),a.scrollTo(0,c))};b.animateScroll=function(b,c,e){var f=t(c?c.getAttribute("data-options"):null),i=l(d||k,e||{},f),m="[object Number]"===Object.prototype.toString.call(b),n=m||!b.tagName?null:b;if(m||n){var o=a.pageYOffset;i.selectorHeader&&!g&&(g=document.querySelector(i.selectorHeader)),h||(h=u(g));var z,A,r=m?b:q(n,h,parseInt(i.offset,10)),w=r-o,x=s(),y=0,B=function(d,e,f){var g=a.pageYOffset;(d==e||g==e||a.innerHeight+g>=x)&&(clearInterval(f),v(b,e,m),i.callback(b,c))},C=function(){y+=16,z=y/parseInt(i.speed,10),z=z>1?1:z,A=o+w*p(i.easing,z),a.scrollTo(0,Math.floor(A)),B(A,r,j)},D=function(){clearInterval(j),j=setInterval(C,16)};0===a.pageYOffset&&a.scrollTo(0,0),D()}};var w=function(c){a.location.hash;e&&(e.id=e.getAttribute("data-scroll-id"),b.animateScroll(e,f),e=null,f=null)},x=function(b){if(0===b.button&&!b.metaKey&&!b.ctrlKey&&(f=n(b.target,d.selector),f&&"a"===f.tagName.toLowerCase()&&f.hostname===a.location.hostname&&f.pathname===a.location.pathname&&/#/.test(f.href))){var c=o(f.hash);if("#"===c){b.preventDefault(),e=document.body;var g=e.id?e.id:"smooth-scroll-top";return e.setAttribute("data-scroll-id",g),e.id="",void(a.location.hash.substring(1)===g?w():a.location.hash=g)}e=document.querySelector(c),e&&(e.setAttribute("data-scroll-id",e.id),e.id="",f.hash===a.location.hash&&(b.preventDefault(),w()))}},y=function(a){i||(i=setTimeout(function(){i=null,h=u(g)},66))};return b.destroy=function(){d&&(document.removeEventListener("click",x,!1),a.removeEventListener("resize",y,!1),d=null,e=null,f=null,g=null,h=null,i=null,j=null)},b.init=function(e){c&&(b.destroy(),d=l(k,e||{}),g=d.selectorHeader?document.querySelector(d.selectorHeader):null,h=u(g),document.addEventListener("click",x,!1),a.addEventListener("hashchange",w,!1),g&&a.addEventListener("resize",y,!1))},b});
