093d91f189a8180c88c846ccbe86534df28813c8
amcllc mktserver digital-ocean <wmeraflen+1@gmail.com>
2017-06-05 22:17:03 +0000

    when you click the tag, the admin modal comes up from behind. Now the modal comes up and the popup goes down (giggidy)

--- laravel/resources/views/layouts/bascom/pages/inc/epop.blade.php
+++ laravel/resources/views/layouts/bascom/pages/inc/epop.blade.php
@@ -1,48 +1,3 @@
-<style type='text/css'>
-.exitpop-inner {
-    display: block;
-    margin: 0 auto;
-    position: relative;
-    text-align: center;
-    background-size: cover
-}
-
-.exitpop-inner .epop-title {
-    background: #85cebc;	/* needs to change */
-    color: #fff;			/* needs to change */
-    padding: 15px;
-    font-size: 40px
-}
-
-.exitpop-inner .epop-message {
-   color: #CF5300; /* Needs to change */
-}
-
-.exitpop-inner .epop-message h1 {
-    font-size: 80px;
-    margin: 80px 0 0;
-    padding: 0;
-    line-height: 60px;
-    font-weight: 800;
-    letter-spacing: -1px;
-    text-transform: uppercase
-}
-
-.exitpop-inner .epop-message h3 {
-    font-size: 30px;
-    letter-spacing: -1px
-}
-
-.exitpop-inner .epop-button {
-    padding: 30px
-}
-
-.exitpop-inner .epop-button .btn {
-    padding: 15px;
-    font-size: 40px;
-    font-weight: 700
-}
-</style>
 <div id="exitpopup-overlay" class="exitpopup-overlay" <?php if (\App\System\Session::isCmsUser()): ?> data-localstorage="ignore"<?php endif;?>>
     <div id="exit_pop" class="exitpop-content text-center">
         <div class="exitpop-inner">
@@ -54,7 +9,7 @@
             <h3 id="epop-message-h3"><?php echo $entity->getText('epopup-message-2', ['oneshot' => 'Sub text goes here']);?></h3>
             </div>
             <div class="epop-button">
-                <a href="/apply-online" class="btn btn-brown btn-mod btn-block">See Floorplans</a>
+                <a href="/floorplans" class="btn btn-brown btn-mod btn-block">See Floorplans</a>
             </div>
         </div>
         <a href="#" class="fa fa-times-circle" id="epop-close"></a>
