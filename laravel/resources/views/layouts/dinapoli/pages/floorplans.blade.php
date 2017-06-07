532e8867ecb2dda3a101f4ad89344722970e5a29
amcllc live <amcllc live>
2017-06-07 17:13:42 -0600

    automated updates

--- laravel/resources/views/layouts/dinapoli/pages/floorplans.blade.php
+++ laravel/resources/views/layouts/dinapoli/pages/floorplans.blade.php
@@ -119,7 +119,7 @@ $js->generateIDs();
                                                     <li>Sq Feet: <b><span><?php echo $object->SQFT; ?></span></b></li>
                                                     <li>Bedrooms: <b><span><?php echo $object->BED; ?></span></b></li>
                                                     <li>Bathrooms: <b><span><?php echo $object->BATH;?></span></b></li>
-                                                    <li>Deposit: <b><span>$100</span></b></li>
+                                                    <li>Deposit: <b><span>$<?=$object->DEPOSIT;?></span></b></li>
                                                 </ul>
                                             </div>
 
